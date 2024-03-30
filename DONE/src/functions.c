efa48b82eb43e45b69bf24d0f71f20e6672114ec
RebelNightmare_ <119054272+Rebel-Nightmare@users.noreply.github.com>
2024-03-30 23:31:28 +0100

    openfile
    dont open files, still needs to be fixed (it crashes)

--- DONE/src/functions.c
+++ DONE/src/functions.c
@@ -112,14 +112,6 @@ void openShell(settings_t *settings)
     settings->node_type == switch_t ? openSwitchShellWrapper() : openNodeShellWrapper(settings->node_name);
 }
 
-void openProject(settings_t *settings)
-{
-}
-
-void saveProject(settings_t *settings)
-{
-}
-
 void clearCanvas(settings_t *settings)
 {
     if (settings->numlink > 0 || settings->numnodes > 0)
@@ -131,4 +123,76 @@ void clearCanvas(settings_t *settings)
         free(interface->nodes);
         free(interface->rectangles);
     }
+}
+
+void openProject(settings_t *settings)      // TODO: fix crash
+{
+    if(settings->isSimulating){  // if an experiment was running we need to kill the containers
+        stop(settings);
+    }   
+
+    clearCanvas(settings);  // first, clearing the canvas
+    
+    FILE *file = fopen("./saves/template.done", "r");
+    int numnodes,numlinks;
+    node_t *nodes;
+    link_t *links;
+
+    if(file != NULL){
+        fscanf(file, "%d\n%d\n", &numnodes, &numlinks);
+        printf("%d %d\n", numnodes, numlinks);
+        printf("%d %d\n", settings->numnodes, settings->numlink);
+
+        settings->numnodes = numnodes;
+        //settings->numlink = numlinks;
+
+        //nodes = (node_t *)malloc(numnodes * sizeof(node_t));
+        //links = (link_t *)malloc(numlinks * sizeof(link_t));
+
+        char name[50];  // buffer
+
+        /* 
+        for(int i=0; i < numnodes; i++){    // reading all nodes
+            fscanf(file,"%s\n%d\n%d %d\n", name, &nodes[i].type, &nodes[i].x, &nodes[i].y);
+            printf("%s\n", name);
+            nodes[i].name = (char *)malloc(strlen(name) * sizeof(char));    // allocating memory for node name
+            //strcpy(name, nodes[i].name);
+            //printf("node name: %s\n", nodes[i].name);
+        }
+        */
+        //for(int i=0; i < numlinks; i++){    // reading all links
+            //fscanf(file,"%s\n%d\n%d %d\n",&nodes[i].name, &nodes[i].type, &nodes[i].x, &nodes[i].y);
+        //}
+    } else {
+        printf("ALERT | There was an error while opening the file. Perhaps the path is wrong?\n");
+    }
+
+}
+
+void saveProject(settings_t *settings)
+{    
+    FILE *file = fopen("./saves/template.done", "w");
+
+    if (file != NULL) {
+        printf("%d\n", settings->numnodes);
+        fprintf(file,"%d\n%d\n",settings->numnodes,settings->numlink);        // saving node number and link number at the top of the file
+
+        interface_t *gui = (interface_t *)(settings->GUIdata);
+
+        for(int i=0; i < settings->numnodes; i++){     // saving every node
+            node_t current_node = gui->nodes[i];
+            fprintf(file,"%s\n%d\n%d %d\n",current_node.name, current_node.type, current_node.x, current_node.y);   // data for every node
+        }
+
+        for(int i=0; i < settings->numlink; i++){   // saving every link
+            link_t current_link = gui->links[i];
+            fprintf(file,"%s\n%s\n%d\n%d\n",current_link.node1,current_link.node2,current_link.node1_type,current_link.node2_type);     // data for every link
+        }
+
+        fclose(file);
+
+    } else {
+        printf("ALERT | There was an error while opening the file. Perhaps the path is wrong?\n");
+    }
+
 }
